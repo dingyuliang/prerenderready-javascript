f5f2328be43d299fc3afa3de9dc69bfac5c16e50
dingyuliang <dingyuliang@msn.com>
2017-02-11 09:20:16 -0800

    Update prerenderwrapper.js

--- src/angular1.x/prerenderwrapper.js
+++ src/angular1.x/prerenderwrapper.js
@@ -1,1 +1,12 @@
+(function (window, angular) {
+    angular.extend(angular, {
+        prerenderInit: function () {
+            window.prerenderReady = false;
+        },
+        prerenderReady: function () {
+            window.prerenderReady = true;
+        }
+    });
 
+    angular.prerenderInit();
+})(window, angular)
