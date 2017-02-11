813c1537244807f969ed643790fbdbb90b2e22b2
dingyuliang <dingyuliang@msn.com>
2017-02-11 09:10:48 -0800

    Update prerenderwrapper.js

--- src/jQuery/prerenderwrapper.js
+++ src/jQuery/prerenderwrapper.js
@@ -1,1 +1,12 @@
+(function (window, $) {
+    $.extend($, {
+        prerenderInit: function () {
+            window.prerenderReady = false;
+        },
+        prerenderReady: function () {
+            window.prerenderReady = true;
+        }
+    });
 
+    $.prerenderInit();
+})(window, jQuery)
